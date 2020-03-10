c824235f0377564d9cab7bb06e9adcbde2913c6c
Kevin R. Moore <kevmoor@sandia.gov>
2020-03-10 15:03:05 -0600

    Modified readMesh to not use fscanf, tests passed

--- deps/OWENSvawtFoa/commonSource/readMesh.m
+++ deps/OWENSvawtFoa/commonSource/readMesh.m
@@ -6,7 +6,7 @@ function [mesh] = readMesh(filename)
 % *             See license.txt for disclaimer information             *
 % **********************************************************************
 %   [mesh] = readMesh(filename)
-%                    
+%
 %   This function reads the mesh file and stores data in the mesh object.
 %
 %      input:
@@ -17,24 +17,32 @@ function [mesh] = readMesh(filename)
 
 fid = fopen(filename,'r');   %open mesh file
 
-temp = fscanf(fid,'%i',2);   %read in number of nodes and number of elements
+% temp = fscanf(fid,'%i',2);   %read in number of nodes and number of elements
+temp = getSplitLine(fid);
 numNodes = temp(1);
 numEl = temp(2);
 
+nodeNum = zeros(numNodes,1);
+x = zeros(numNodes,1);
+y = zeros(numNodes,1);
+z = zeros(numNodes,1);
+
+conn = zeros(numEl,2);
+elNum = zeros(numEl,1);
+
 for i=1:numNodes            % read in node number and node coordinates
-   nodeNum(i) = fscanf(fid,'%i',1);
-   temp = fscanf(fid,'%f',3);
-   x(i) = temp(1);
-   y(i) = temp(2);
-   z(i) = temp(3);
+    temp = getSplitLine(fid);
+    nodeNum(i) = temp(1);
+    x(i) = temp(2);
+    y(i) = temp(3);
+    z(i) = temp(4);
 end
 
 for i=1:numEl               % read in element number and connectivity list
-   elNum(i) = fscanf(fid,'%i',1);
-   dum = fscanf(fid,'%i',1);
-   conn(i,:) = fscanf(fid,'%i',2);
-   
-   
+    temp = getSplitLine(fid);
+    elNum(i) = temp(1);
+
+    conn(i,:) = temp(3:4);
 end
 
 fclose(fid);  %close mesh file
@@ -47,5 +55,19 @@ mesh.y = y;
 mesh.z = z;
 mesh.elNum = elNum;
 mesh.conn = conn;
+
+end
+
+function [data] = getSplitLine(fid)
+
+line = myfgetl(fid);
+% Find where all of the delimiters are
+delimiter_idx = find(line == '	'); %Tab delimited
+delimiter_idx = [0.0,delimiter_idx,length(line)+1];
+% Extract the data from the beginning to the last delimiter
+data = zeros(length(delimiter_idx)-1,1);
+for k = 2:length(delimiter_idx)
+    data(k-1) = str2double(line(delimiter_idx(k-1)+1:delimiter_idx(k)-1));
+end
 
 end
