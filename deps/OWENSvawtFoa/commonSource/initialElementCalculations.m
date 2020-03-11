c1fccebe705274d095f49212ecfa77df989e7e6a
Kevin R. Moore <kevmoor@sandia.gov>
2020-03-11 11:50:31 -0600

    Syntax Issues, tests passed

--- deps/OWENSvawtFoa/commonSource/initialElementCalculations.m
+++ deps/OWENSvawtFoa/commonSource/initialElementCalculations.m
@@ -6,7 +6,7 @@ function [elStorage] = initialElementCalculations(model,el,mesh)
 % *             See license.txt for disclaimer information             *
 % **********************************************************************
 %   [elStorage] = initialElementCalculations(model,el,mesh)
-%                    
+%
 %   This function performs initial element calculation for use later in
 %   analysis for efficiency gains.
 %
@@ -22,38 +22,42 @@ function [elStorage] = initialElementCalculations(model,el,mesh)
 %initial element calculation
 numNodesPerEl = 2;
 
+elStorage = cell(1,mesh.numEl);
 for i=1:mesh.numEl
-        %Calculate Ke and Fe for element i
-        elInput.elementOrder = model.elementOrder; %assign elInput for element i
-        elInput.modalFlag = true;
-        elInput.xloc = [0.0 el.elLen(i)];
-        elInput.sectionProps = el.props{i};
-        elInput.sweepAngle = el.psi(i);
-        elInput.coneAngle = el.theta(i);
-        elInput.rollAngle = el.roll(i);
-        elInput.aeroSweepAngle = 0.0;
-
-        for j=1:numNodesPerEl
-            
-            %get element cooridnates
-            elx(j) = mesh.x(mesh.conn(i,j));
-            ely(j) = mesh.y(mesh.conn(i,j));
-            elz(j) = mesh.z(mesh.conn(i,j));
-            
-        end
+    %Calculate Ke and Fe for element i
+    elInput.elementOrder = model.elementOrder; %assign elInput for element i
+    elInput.modalFlag = true;
+    elInput.xloc = [0.0 el.elLen(i)];
+    elInput.sectionProps = el.props{i};
+    elInput.sweepAngle = el.psi(i);
+    elInput.coneAngle = el.theta(i);
+    elInput.rollAngle = el.roll(i);
+    elInput.aeroSweepAngle = 0.0;
     
-        %get concentrated terms associated with elemetn
-         [massConc,stiffConc,loadConc,model.joint,nodalTerms.concMass,nodalTerms.concStiff] = ConcMassAssociatedWithElement(mesh.conn(i,:),model.joint,model.nodalTerms.concMass,model.nodalTerms.concStiff,model.nodalTerms.concLoad);
-     
-        elInput.x = elx;
-        elInput.y = ely;
-        elInput.z = elz;
-        elInput.concMassFlag = ~isempty(find(massConc,1));
-        elInput.concMass = massConc; %only needed for structure mass props (not used in saved element matrices)
+    elx = zeros(1,numNodesPerEl);
+    ely = zeros(1,numNodesPerEl);
+    elz = zeros(1,numNodesPerEl);
+    for j=1:numNodesPerEl
         
-        elInput.Omega = 0;
-                
-        [elStorage{i}] = calculateTimoshenkoElementInitialRun(elInput); %initial element calculations for storage
+        %get element cooridnates
+        elx(j) = mesh.x(mesh.conn(i,j));
+        ely(j) = mesh.y(mesh.conn(i,j));
+        elz(j) = mesh.z(mesh.conn(i,j));
+        
+    end
+    
+    %get concentrated terms associated with elemetn
+    [massConc,~,~,model.joint,nodalTerms.concMass,nodalTerms.concStiff] = ConcMassAssociatedWithElement(mesh.conn(i,:),model.joint,model.nodalTerms.concMass,model.nodalTerms.concStiff,model.nodalTerms.concLoad);
+    
+    elInput.x = elx;
+    elInput.y = ely;
+    elInput.z = elz;
+    elInput.concMassFlag = ~isempty(find(massConc,1));
+    elInput.concMass = massConc; %only needed for structure mass props (not used in saved element matrices)
+    
+    elInput.Omega = 0;
+    
+    [elStorage{i}] = calculateTimoshenkoElementInitialRun(elInput); %initial element calculations for storage
 end
 
 end
