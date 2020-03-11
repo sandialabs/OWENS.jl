45147471f89072863d8acb7c4ebef901af5e7c42
Kevin R. Moore <kevmoor@sandia.gov>
2020-03-11 09:57:38 -0600

    Editor synatx issues resolution, test passed

--- deps/OWENSvawtFoa/modalSource/staticExec.m
+++ deps/OWENSvawtFoa/modalSource/staticExec.m
@@ -6,7 +6,7 @@ function staticExec(model,mesh,el,displ,Omega,OmegaStart)
 % *             See license.txt for disclaimer information             *
 % **********************************************************************
 %   staticExec(model,mesh,el,displ,Omega,OmegaStart,fid)
-%                    
+%
 %   This function executes static analysis.
 %
 %   input:
@@ -16,24 +16,24 @@ function staticExec(model,mesh,el,displ,Omega,OmegaStart)
 %   displ          = displacement vector for use in pre-stressed analysis
 %   Omega          = rotor speed (Hz)
 %   OmegaStart     = rotor speed (Hz) from previous analysis if stepping
-%                    through various rotor speeds, may be useful in load 
+%                    through various rotor speeds, may be useful in load
 %                    stepping
 %
 %   output:        (NONE)
 
-    [elStorage] = initialElementCalculations(model,el,mesh); %performs initial element calculations
+[elStorage] = initialElementCalculations(model,el,mesh); %performs initial element calculations
 
-    [structureMass,structureMOI,structureMassCenter]=calculateStructureMassProps(elStorage); %calculate mass properties of structure
+%     [structureMass,structureMOI,structureMassCenter]=calculateStructureMassProps(elStorage); %calculate mass properties of structure
 
-    %Do nonlinear iteration if needed
-    model.aeroElasticOn = false;
-    model.aeroForceOn = true;
-    [displ,elStrain,staticAnalysisSuccessful]=staticAnalysis(model,mesh,el,displ,Omega,OmegaStart,elStorage); %performs static analysis about specified operating condition
-    
-    if(staticAnalysisSuccessful)
-        save(model.outFilename,'displ','elStrain'); %saves static analysis displacement output
-    else
-        error('Static analysis unsuccessful. Exiting');
-    end
+%Do nonlinear iteration if needed
+model.aeroElasticOn = false;
+model.aeroForceOn = true;
+[displ,elStrain,staticAnalysisSuccessful]=staticAnalysis(model,mesh,el,displ,Omega,OmegaStart,elStorage); %performs static analysis about specified operating condition
+
+if(staticAnalysisSuccessful)
+    save(model.outFilename,'displ','elStrain'); %saves static analysis displacement output
+else
+    error('Static analysis unsuccessful. Exiting');
+end
 end
 
