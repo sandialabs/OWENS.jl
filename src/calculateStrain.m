f12568c45817d47a68784d0ffc77b3b6ae801692
Kevin R. Moore <kevmoor@sandia.gov>
2020-07-13 10:21:39 -0600

    Rename file extensions

--- src/calculateStrain.m
+++
@@ -1,7 +0,0 @@
-function [eps_xx,gam_xy,gam_xz] = calculateStrain(strain,gpNum,y,z)
-	
-	eps_xx = strain.eps_xx_0(gpNum) + strain.eps_xx_y(gpNum)*y + strain.eps_xx_z(gpNum)*z;
-    gam_xy = strain.gam_xy_0(gpNum) + strain.gam_xy_z(gpNum)*z;
-    gam_xz = strain.gam_xz_0(gpNum) + strain.gam_xz_y(gpNum)*y;
-		
-end
