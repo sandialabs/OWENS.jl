01557eaf923deaf0bda64a93adebfe27dfb6a19f
kbonney <kirkb1998@gmail.com>
2025-08-06 15:20:49 -0400

    clean up example

--- examples/literate/B_detailedInputs.jl
+++ examples/literate/B_detailedInputs.jl
@@ -18,36 +18,24 @@
 import OWENS
 import OWENSAero
 
-#### import PyPlot
 runpath = path = "/home/runner/work/OWENS.jl/OWENS.jl/examples/literate" # to run locally, change to splitdir(@__FILE__)[1]
 # runpath = path =  splitdir(@__FILE__)[1]
 
-# Load the unified options using ModelingOptions with the complete YAML file
+# Load options using ModelingOptions with the complete YAML file
 modelingOptions = OWENS.ModelingOptions("$runpath/sampleOWENS_complete.yml", path=path)
+
+# Preprocess the modeling options to generate setup_options
+setup_options = OWENS.preprocess_modeling_options_setup(modelingOptions, path)
 
 # Call the helper function that builds the mesh, calculates the sectional properties,
 # and aligns the sectional properties to the mesh elements
 # We can now use the ModelingOptions struct directly with setupOWENS
-
-# Run preprocessing function to generate setup_options from modelingOptions
-setup_options = OWENS.preprocess_modeling_options_setup(modelingOptions, path)
-
-# Use the new struct output format
 setup_outputs = OWENS.setupOWENS(
     path;
     setup_options = setup_options,
     verbosity = 1,
     return_componentized = true
 )
-
-nothing
-
-# Optionally, we can run the finite element solver with gemetrically exact beam theory via GXBeam.jl
-# this requires that the OWENS style inputs are converted to the GXBeam inputs.  This interface also
-# includes the ability to output VTK files, which can be viewed in paraview.  We have adapted this interface
-# to work with OWENS inputs as well.
-
-nothing
 
 # If the sectional properties material files includes cost information, that is combined with the density 
 # to estimate the overall material cost of of materials in the blades
@@ -73,62 +61,9 @@ println("Total Material Cost Blades: \$$(sum(mass_breakout_blds.*plyprops_bld.co
 println("Total Material Cost Tower: \$$(sum(mass_breakout_twr.*plyprops_twr.costs))")
 println("Total Material Cost: \$$(sum(mass_breakout_blds.*plyprops_bld.costs)+ sum(mass_breakout_twr.*plyprops_twr.costs))")
 
+# Run the unsteady simulation
 
-nothing
-
-# Here we apply the boundary conditions.  For this case, with a regular cantelever tower, the tower base node which is 
-# 1 is constrained in all 6 degrees of freedom to have a displacement of 0.  You can change this displacement to allow for things
-# like pretension, and you can apply boundary conditions to any node.
-
-# pBC = [1 1 0
-# 1 2 0
-# 1 3 0
-# 1 4 0
-# 1 5 0
-# 1 6 0]
-
-nothing
-
-# There are inputs for the overall coupled simulation, please see the api reference for specifics on all the options
-
-
-# inputs = OWENS.Inputs(;
-#     verbosity = modelingOptions.OWENS_Options.verbosity,
-#     analysisType = modelingOptions.OWENS_Options.structuralModel,
-#     tocp = [0.0,100000.1],
-#     Omegaocp = [modelingOptions.OWENSAero_Options.RPM,modelingOptions.OWENSAero_Options.RPM] ./ 60,
-#     tocp_Vinf = [0.0,100000.1],
-#     Vinfocp = [modelingOptions.OWENSAero_Options.Vinf,modelingOptions.OWENSAero_Options.Vinf],
-#     numTS = modelingOptions.OWENS_Options.numTS,
-#     delta_t = modelingOptions.OWENS_Options.delta_t,
-#     AD15On = modelingOptions.OWENS_Options.AeroModel=="AD",
-#     aeroLoadsOn = modelingOptions.OWENS_Options.aeroLoadsOn
-# )
-
-nothing
-
-# Then there are inputs for the finite element models, also, please see the api reference for specifics on the options (TODO: ensure that this is propogated to the docs)
-# Use OWENSFEA_Options for FEA model parameters
-# fea_options = modelingOptions.OWENSFEA_Options
-
-# feamodel = OWENS.FEAModel(;
-#     analysisType = modelingOptions.OWENS_Options.structuralModel,
-#     dataOutputFilename = "none",
-#     joint = myjoint,
-#     platformTurbineConnectionNodeNumber = 1,
-#     pBC,
-#     nlOn = fea_options.nlOn,
-#     numNodes = mymesh.numNodes,
-#     RayleighAlpha = 0.05,
-#     RayleighBeta = 0.05,
-#     iterationType = "DI")
-
-nothing
-
-# Here is where we actually call the unsteady simulation and where owens pulls the aero and structural solutions together
-# and propogates things in time.
-
-println("Running Unsteady")
+println("Running Unsteady Simulation")
 
 unsteady_outputs = OWENS.Unsteady_Land(setup_outputs, modelingOptions, returnold=false)
 
@@ -137,8 +72,6 @@ if modelingOptions.OWENS_Options.AeroModel == "AD"
     OWENS.OWENSOpenFASTWrappers.endTurb()
 end
 
-nothing
-
 # Like described above, we can output vtk files viewable in paraview.  Here it is done for each time step and shows the 
 # deformations.  Additionaly, there is a method to input custom values and have them show up on the vtk surface mesh
 # for example, strain, or reaction force, etc.  This is described in more detail in the api reference for the function and: TODO
@@ -146,42 +79,8 @@ nothing
 println("Saving VTK time domain files")
 OWENS.OWENSFEA_VTK("$path/vtk/SNLARCUS5MW_timedomain_TNBnltrue", unsteady_outputs, setup_outputs; scaling=1)
 
-nothing
-
-
-#### Populate Components with Strain Data
-
-# Populate the components with strain data from the unsteady simulation
-for icomp in 1:length(setup_outputs.components)
-    component = setup_outputs.components[icomp]
-    
-    # Get the element numbers for this component
-    startE = component.elNumbers[1]
-    stopE = component.elNumbers[end]
-    
-    println("Populating strain data for component: $(component.name)")
-    println("  Element range: $startE to $stopE")
-    println("  Strain history dimensions: $(size(unsteady_outputs.epsilon_x_hist))")
-    
-    # Extract strain data for this component's elements
-    # The strain history arrays are organized as [4, element, time_step] where 14 is quadrature points
-    # We extract the first quadrature point for each element: [1, startE:stopE, :] gives [element, time_step]
-    # safetyfactor_fatigue expects [element, time_step] format
-    component.e_x = unsteady_outputs.epsilon_x_hist[1, startE:stopE, :]  # [element, time_step]
-    component.e_y = unsteady_outputs.epsilon_y_hist[1, startE:stopE, :]  # [element, time_step] 
-    component.e_z = unsteady_outputs.epsilon_z_hist[1, startE:stopE, :]  # [element, time_step]
-    component.k_x = unsteady_outputs.kappa_x_hist[1, startE:stopE, :]    # [element, time_step]
-    component.k_y = unsteady_outputs.kappa_y_hist[1, startE:stopE, :]    # [element, time_step]
-    component.k_z = unsteady_outputs.kappa_z_hist[1, startE:stopE, :]    # [element, time_step]
-    
-    println("  Populated strain data dimensions: $(size(component.e_x))")
-end
-
-nothing
-
-#### Ultimate Failure #####
-
 # Create PostProcessingOptions
+
 postprocess_options = OWENS.PostProcessOptions(
     verbosity=modelingOptions.OWENS_Options.verbosity,
     usestationBld=0,
@@ -197,5 +96,3 @@ components = OWENS.safetyfactor_fatigue(
     modelingOptions.OWENS_Options.delta_t;
     options=postprocess_options
 )
-
-nothing
